3c5c762721be04731528fe4c3238745c6283e9f4
Pedro Augusto Torres Bento <127890688+pedroaugtb@users.noreply.github.com>
2025-06-14 13:36:50 -0300

    Commit 9 - Criando Arquivos

--- index.js
+++ index.js
@@ -1,24 +1,8 @@
 const { readFileSync } = require("fs");
+
 const Repositorio = require("./repositorio");
 const ServicoCalculoFatura = require("./servico");
-
-function formatarMoeda(valor) {
-  return new Intl.NumberFormat("pt-BR",
-    { style: "currency", currency: "BRL", minimumFractionDigits: 2 })
-    .format(valor / 100);
-}
-
-function gerarFaturaStr(fatura, calc) {
-  let faturaStr = `Fatura ${fatura.cliente}\n`;
-  for (let apre of fatura.apresentacoes) {
-    faturaStr += `  ${calc.repo.getPeca(apre).nome}: `
-               + `${formatarMoeda(calc.calcularTotalApresentacao(apre))} `
-               + `(${apre.audiencia} assentos)\n`;
-  }
-  faturaStr += `Valor total: ${formatarMoeda(calc.calcularTotalFatura(fatura.apresentacoes))}\n`;
-  faturaStr += `Créditos acumulados: ${calc.calcularTotalCreditos(fatura.apresentacoes)} \n`;
-  return faturaStr;
-}
+const gerarFaturaStr = require("./apresentacao");
 
 const faturas = JSON.parse(readFileSync("./faturas.json"));
 const calc = new ServicoCalculoFatura(new Repositorio());
